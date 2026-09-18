49e0815fb69d417df910cc01e612e8c8ae4648b5
Daniel Barlow <dan@telent.net>
2026-09-18 22:57:32 +0100

    libubootenv: hack cmake version

--- pkgs/libubootenv/default.nix
+++ pkgs/libubootenv/default.nix
@@ -13,6 +13,10 @@ stdenv.mkDerivation {
     rev = "3f4d15e36ceb58085b08dd13f3f2788e9299877b"; # v0.3.5
     hash = "sha256-i7gUb1A6FTOBCpympQpndhOG9pCDA4P0iH7ZNBqo+PA=";
   };
+  preConfigure = ''
+    sed -i.bak 's/cmake_minimum_required.*/cmake_minimum_required (VERSION 4.1.2)/' CMakeLists.txt
+    sed -i.bak 's/cmake_minimum_required.*/cmake_minimum_required (VERSION 4.1.2)/' src/CMakeLists.txt
+  '';
   buildInputs = [
     zlib
     libyaml
